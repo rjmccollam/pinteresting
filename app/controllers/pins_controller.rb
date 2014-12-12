18beb0f48a994c1d8050c695dd334ff816ab724e
RJ McCollam <rj@wedodev.com>
2014-12-11 19:36:00 -0600

    fixed issue when deleting pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -25,19 +25,16 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     @pin.save
-    flash[:notice] = 'Pin was successfully created.' if @pin.save
     respond_with(@pin)
   end
 
   def update
     @pin.update(pin_params)
-    flash[:notice] = 'Pin was successfully updated.' if @pin.save
     respond_with(@pin)
   end
 
   def destroy
     @pin.destroy
-    flash[:notice] = 'Pin was successfully deleted.' if @pin.save
     respond_with(@pin)
   end
 
